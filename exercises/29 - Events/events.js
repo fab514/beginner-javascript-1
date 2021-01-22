1dd128401d6880c716c55749dcc8a351b1e74fc7
Felicia Brenner <felicia.brenner514@gmail.com>
2021-01-22 13:25:55 -0600

    Added a button and a event listener.

---
+++ exercises/29 - Events/events.js
@@ -0,0 +1,8 @@
+const butts = document.querySelector('.butts');
+// Event Listener steps: look for something, listen for something, do something.
+butts.addEventListener('click', function (){
+    console.log('Butts...It Got Clicked!!!!');
+});
+// most common event
+// callback function regular function that is called back by add event listener. Browser calls the function instead of the user.
+// a anonomous function like above has no name. 
