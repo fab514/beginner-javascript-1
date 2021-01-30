80aeeba6ee1875ca646da715d8c1fd0e6fe5dbea
Felicia Brenner <felicia.brenner514@gmail.com>
2021-01-29 19:26:15 -0600

    added 2 methods of changing panels within the tabs.

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,7 +1,7 @@
 const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('[role="tab"]');
 // searches for all the tabs instead of within the document. Good for reusing the JS file 
-const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
 
 function handleTabClick(event) {
     // Hide all tab panels. Easier to start with hiding them all as the first step. 
@@ -18,11 +18,25 @@ function handleTabClick(event) {
     event.currentTarget.setAttribute('aria-selected', true);
     // Find the associated tabPanel and show it!
     const { id } = event.currentTarget;
+    /*
+    Method 1
+
     const tabPanel = tabs.querySelector(`
-    [aria-labeledby="ruby"]`);
+    [aria-labeledby="${id}"]`);
     console.log(tabPanel);
     tabPanel.hidden = false;
+    */
+   // Method 2- Find in the array of tabPanels. This is a slightly preffered method in place of query selector. 
+   console.log(tabPanels);
+   const tabPanel = tabPanels.find(
+      panel => panel.getAttribute('aria-labeledby') === id
+   ); 
+   tabPanel.hidden = false;
+   
+   // a find method is only used on arrays. 
+    
 }
+
 
 tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
     
