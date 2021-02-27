d9057c6568057c93686216f5643916deec5d4793
Felicia Brenner <felicia.brenner514@gmail.com>
2021-02-27 08:29:50 -0600

    Transferred information from the local files to the the function file attached to github, made no alterations from original

---
+++ playground/functions/practice.js
@@ -0,0 +1,13 @@
+// Function Definition //
+
+function carMan() {
+    return `Hello, I am ${firstName} I like cars`;
+  }
+  
+  function feliciaDaBombDiggity() {
+    console.log('Keeps gaming group in shape');
+    console.log('Plays many instruments');
+    console.log('She makes everyone around her smile.');
+    return "That's why she get's the best chair at game night";
+  }
+  
