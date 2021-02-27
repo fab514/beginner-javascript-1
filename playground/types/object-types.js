3a4eea60957b7df8b744240a46c1879709200976
Felicia Brenner <felicia.brenner514@gmail.com>
2021-02-27 08:18:57 -0600

    Transferred notes and code from the local files to the folder connected to github. Only made small changes to the notes and code from the original version.

---
+++ playground/types/object-types.js
@@ -0,0 +1,14 @@
+// The Object class represents one of JavaScript's data types. It is used to store various keyed 
+// collections and more complex entities. Objects can be created using the Object() constructor or the 
+// object initializer / literal syntax.
+
+
+console.log('object here');
+// Object (person) with sub-variables
+
+const person = {
+  first: 'felicia',
+  last: 'brenner',
+  age: 100,
+};
+// We will continue to learn objects at a later time.
