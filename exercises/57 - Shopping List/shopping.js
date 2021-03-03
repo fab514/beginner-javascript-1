ee02283d08e613dc4554422a3ffb1fb4019c3aa9
Felicia Brenner <felicia.brenner514@gmail.com>
2021-03-02 22:19:15 -0600

    Made two new functions in shopping.js. One is to mirror saved items to the browsers local storage by turning it into a string first,  and the other function is to restore the information from the local storage and convert back to an array.

--- exercises/57 - Shopping List/shopping.js
+++ exercises/57 - Shopping List/shopping.js
@@ -24,7 +24,9 @@ function handleSubmit(e) {
     console.log(`There are now ${items.length} in your state`);
     // clear the form
     e.target.reset(); // Can use target or current target since there is no bubbling that will need current target. Look UP
-    displayItems();
+    // fire off a custom event that will tell anyone who cares that the items have been updated!
+    list.dispatchEvent(new CustomEvent('itemsUpdated'));
+    // a dispatch event lives on all DOM elements. 
 }
 
 function displayItems() {
@@ -39,5 +41,26 @@ function displayItems() {
     // aria label will allow people who are using readers to hear the item they will be marking or removing. 
 }
 
+function mirrorToLocalStorage() {
+    console.info('Saving items to local storage')
+    localStorage.setItem('items', JSON.stringify(items)); // before reaching local storage, JSON.stringify will convert an array or ocject into a string. 
+}
+// Saving to a local storage will allow the user to come back to the browser and still see the information in their history. 
+// Local storage uses text only, if you try adding a different type it will convert it to a string. 
+
+function restoreFromLocalStorage() {
+    console.info('Restoring from LS');
+    const lsItems = JSON.parse(localStngorage.getItem('items')); // JSON.parse will turn items converted into a string back into an array. 
+    if(lsItems.length) {
+        // The following are different wasys to bring the items into a string. ...lsItems are spreading the items.
+        // items = lsItems;
+        // lsItems.forEach(item => items.push(item));
+        // items.push(lsItems[0], lsItems[1]);
+        items.push(...lsItems) // This would be my preffered method over the others. 
+        list.dispatchEvent(new CustomEvent('itemsUpdated'));
+    }
+}
 // whenever you want an event to listen for a submit you do not need to use event click etc. 
 shoppingForm.addEventListener('submit', handleSubmit);
+list.addEventListener('itemsUpdated', displayItems);
+list.addEventListener('itemsUpdated', mirrorToLocalStorage);
