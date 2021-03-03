0f36556029c80c01ae0c518ff3d6589520afeba3
Felicia Brenner <felicia.brenner514@gmail.com>
2021-03-03 11:52:43 -0600

    feat: gallery function that connects to the images in the galler. bug: code breaks at line 9

---
+++ exercises/58 - Gallery/gallery.js
@@ -0,0 +1,32 @@
+function Gallery(gallery) {
+    if (!gallery) {
+        throw new Error('No Gallery Found!');
+    }
+
+    // select the element we need
+    const images = Array.from(gallery.querySelectorAll('img'));
+    const modal = document.querySelectorAll('.modal'); // Reminder that a modal is a dialogue box or pop up window. In this case shows up when you click a picture to make it bigger. 
+    const prevButton = modal.querySelector('.prev');
+    const nextButton = modal.querySelector('.next');
+
+    function showImage(el) {
+        if(!el) {
+            console.info('no image to show');
+            return;
+        }
+        // update the modal with this info
+        console.log(el);
+    }
+
+    function handleImageClick(event) {
+        showImage(event.currentTarget);
+    }
+
+    images.forEach(image => image.addEventListener('click', handleImageClick));
+}
+// Allow to use the same functions by using closure. You can still access a function after it has been run. 
+
+// Use it on the page
+
+const gallery1 = Gallery(document.querySelector('.gallery1'));
+const gallery2 = Gallery(document.querySelector('.gallery2'));
