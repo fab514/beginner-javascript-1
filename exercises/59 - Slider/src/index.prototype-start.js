e0bb6bb0f7dd4abd37f035f0ab61d5e3d9f34148
Felicia Brenner <felicia.brenner514@gmail.com>
2021-03-07 21:42:34 -0600

    fix(this): inside the function slider scope was not needed so I deleted it. Change prevButton and nextButton back into variables

--- exercises/59 - Slider/src/index.prototype-start.js
+++ exercises/59 - Slider/src/index.prototype-start.js
@@ -2,15 +2,18 @@ function Slider(slider) {
     if (!(slider instanceof Element)) {
         throw new Error('No slider passed in');
     }
-    // change the variables to this.name
-    this.prev; 
-    this.current;
-    this.next;
+    // do not need to add properties outside because you can add them at anytime. 
+    
+    
     // change the variables and possibly the query selector to this: this.slides and this.slider.querySelector() 
-    this.slides = this.slider.querySelector('.slides');
-    this.prevButton = this.slider.querySelector('.goToPrev');
-    this.nextButton = this.slider.querySelector('.goToNext');
-
+    this.slides = slider.querySelector('.slides');
+    this.slider = slider; // if we are passing in slider in line 1 we need to call for the query selector later. 
+    const prevButton = slider.querySelector('.goToPrev'); // add event listener is inside this scope so it can stay a
+    const nextButton = slider.querySelector('.goToNext');
+    
+    // when this slider is created, run the start slider function
+    this.startSlider();
+    this.applyClasses();
    
     // Keep event listeners in the Slider function's scope. 
     // Event listeners
@@ -20,20 +23,21 @@ function Slider(slider) {
 }
  // Take all below functions outside of the Slider function
     // put this. in front to make properties.
-    function startSlider() {
+    // Put the main function name in this case Slider.prototype.name = function()
+
+    Slider.prototype.startSlider = function() {
         this.current = this.slider.querySelector('.current') || this.slides.firstElementChild;
         this.prev = this.current.previousElementSibling || this.slides.lastElementChild;
         this.next = this.current.nextElementSibling || this.slides.firstElementChild;
-        console.log({ current, prev, next });
     }
 
-    function applyClasses() {
+    Slider.prototype.applyClasses = function() {
         this.current.classList.add('current');
         this.prev.classList.add('prev');
         this.next.classList.add('next');
     }
 
-    function move(direction) {
+    Slider.prototype.move = function(direction) {
         // first strip all the classes of the current slide
         this.classesToRemove = ['prev', 'current', 'next'];
         this.prev.classList.remove(...classesToRemove);
@@ -59,9 +63,6 @@ function Slider(slider) {
         this.applyClasses();
     }
     
-    // when this slider is created, run the start slider function
-    startSlider();
-    applyClasses();
 
 // keep const here but change to new Slider
 const mySlider = new Slider(document.querySelector('.slider'));
