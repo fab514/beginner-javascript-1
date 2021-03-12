8e43693d687a5f9be1c7d5460d158dc42f85ac4a
Felicia Brenner <felicia.brenner514@gmail.com>
2021-03-12 11:20:01 -0600

    feat(user data arrray): created an array with popup questions in order that will be put in the data array

--- exercises/72 - Async Prompts/scripts.js
+++ exercises/72 - Async Prompts/scripts.js
@@ -1,4 +1,6 @@
 // in async prompts you can only have one input box and it blocks everything else on the page. 
+
+// async and await keywords enable asynchonous, promise-based behavior for promise chains to be written in a cleaner style. This avoids need of explicitly configure promise chains (promise nesting)
 const wait = (ms = 0) => new Promise(resolve => setTimeout(resolve, ms));
 
 async function destroyPopup(popup) { 
@@ -14,6 +16,7 @@ async function destroyPopup(popup) {
 function ask(options) {
     return new Promise(async function(resolve) {
         // First we need to create a popup with all the fields in it
+        // async put in the parent function of 'await' not the ask function
         const popup = document.createElement('form'); 
         popup.classList.add('popup');
         popup.insertAdjacentHTML(
@@ -34,7 +37,16 @@ function ask(options) {
             skipButton.textContent = 'Cancel';
             console.log(popup.firstChild);
             popup.firstElementChild.appendChild(skipButton);
+
             // TODO; listen for a click on that cancel button
+            skipButton.addEventListener(
+                'click', 
+                function() {
+                    resolve(null); // explicently nothing
+                    destroyPopup(popup);
+                },
+                { once: true } // only listened to once
+            );
         }
         // Third, listen for the submit event on the inputs
         popup.addEventListener(
@@ -57,4 +69,57 @@ function ask(options) {
         popup.classList.add('open');
         }); // puts at end of event loop
 }
+
+// select all buttons that have a question
+async function askQuestion(e) {
+    const button = e.currentTarget;
+    const cancel = button.hasAttribute('data-cancel'); 
     
+    const answer = await ask({ 
+        title: button.dataset.question, cancel: true });
+        cancel,
+    console.log(answer);
+}
+const buttons = document.querySelectorAll('[data-question]');
+buttons.forEach(button => button.addEventListener('click', askQuestion));
+
+const questions = [
+    { title: 'What is your name?' },
+    { title: 'What is your age?' },
+    { title: 'What is your dogs name?' },
+];
+
+/*
+Promise.all(questions.map(ask)).then(data => { // ask makes the map array a promise which is wrapped with a new promise.all that listens with a 'then' return data
+    console.log(data); // log an array of data
+}); // array of the three sequential promises in order. 
+*/
+
+function asyncMap(array, callback) { // can be used multiple times. flexible to many different arrays
+    // make an array to store results
+    const results = [];
+    // loop over our array
+    for (const item of array) {
+        results.push(await callback(item));
+    }
+    // when done with the loop return it
+    return results;
+}
+
+async function go() {
+    const answers = await asyncMap(question, ask);
+    console.log(answers);
+}
+
+go()
+
+/*
+async function askMany() { // use 'for 'of' loop 
+    for (const question of questions) { // 'for' 'of' allows you to pause a loop for await inside of it
+        const answer = await ask(question);
+        console.log(answer);
+    }
+}
+
+askMany();
+*/
