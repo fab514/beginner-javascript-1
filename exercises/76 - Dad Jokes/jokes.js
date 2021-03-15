f6ac93519db81492a0648ca1621a219642d9871b
Felicia Brenner <felicia.brenner514@gmail.com>
2021-03-15 12:23:21 -0500

    feat(button text): attached the button text to the joke generator, had the text generate at random and not repeated

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -1,3 +1,6 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+
 const buttonText = [
   'Ugh.',
   '🤦🏻‍♂️',
@@ -8,3 +11,35 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+
+async function fetchJoke() {
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const data = await response.json();
+  return data;
+}
+
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)]; // if you pass arr as an argument you can use this code for any array
+  if(item === not) { // when the array generates the same response right after the function will run again until there is not a repeated answer.
+    console.log('Ahh we used that one last time, look again');
+    return randomItemFromArray(arr, not);
+  }
+  return item;
+}
+
+  async function handleClick() {
+  const { joke } = await fetchJoke();
+  jokeHolder.textContent = joke;
+  jokeButton.textContent = randomItemFromArray(
+    buttonText);
+}
+
+jokeButton.addEventListener('click', handleClick);
+
+
+// A header is additional information that comes along with a request.
+// This information can include the request URL, request method, status code, remote address and Referrer policy along with data that comes back.
