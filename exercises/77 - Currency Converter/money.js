4930cb56639357c8cc0d86b45b9f1c45cb3a5809
Felicia Brenner <felicia.brenner514@gmail.com>
2021-03-15 21:08:16 -0500

    fix(money.js): fix semicolon bug in the variables ratesByBase, also eleminated object data in ratesByBase

--- exercises/77 - Currency Converter/money.js
+++ exercises/77 - Currency Converter/money.js
@@ -1,5 +1,7 @@
 const fromSelect = document.querySelector('[name="from_currency"]');
 const toSelect = document.querySelector('[name="to_currency"]');
+const endpoint = 'https://api.exchangeratesapi.io/latest';
+const ratesByBase = {};
 
 const currencies = {
   USD: 'United States Dollar',
@@ -36,15 +38,42 @@ const currencies = {
   EUR: 'Euro',
 };
 
-function generateOptions(options) {
-  return Object.entries(options).map(
+function generateOptions(options) { // gives a dropdown menu of the different options
+  return Object.entries(options)
+    .map(
     ([currencyCode, currencyName]) =>
     `<option value="${currencyCode}">${currencyCode} - 
     ${currencyName}</option>`
   ).join('');
 }
 
+async function fetchRates(base = 'USD') { // needs a baserate
+  const res = await fetch(`${endpoint}?base=${base}`); // endpoint is the base at the endrs
+  const rates = await res.join();
+  return rates;
+}
+
+async function convert(amount, from, to) {
+  // first check if we even have the rates to convert from that currency
+  if(!ratesByBase[from]) {
+    console.log(`Oh no, we dont have ${from} to convert to 4{to}. So gets go 
+    get it!`
+    );
+    const rates = fetchRates(from);
+    console.log(rates);
+    // store the rates for next time. 
+    ratesByBase[from] = rates;
+  }
+  // convert the amount they pass in
+  const rate = ratesByBase[from].rates[to];
+  const convertedAmount = rate * amount;
+  console.log(`${amount} ${from} is ${convertedAmount} in ${to}`); // will show as 100 CAD is 75.175... in USD
+  return convertedAmount;
+}
+
 const optionsHTML = generateOptions(currencies);
 // on page load populate the option elements
 fromSelect.innerHTML = optionsHTML;
 toSelect.innerHTML = optionsHTML;
+
+// some api cap the amount of rates you can do per hour 
